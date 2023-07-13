952d93b179f2e3be14603987cc2a48e4eb098341
vaji2003 <suuus1287@gmail.com>
2023-07-13 12:55:12 +0530

    add

---
+++ D11-20230707/assignment/p28.py
@@ -0,0 +1,43 @@
+print("Ye oldy keychain shop")
+curr_keychn=0
+cost=10
+sales_tax=0.0825
+ship_cost=5
+per_cost=1
+def add_key():
+    global curr_keychn
+    add=int(input(f"\nYou have {curr_keychn} keychains.How many to add:"))
+    curr_keychn+=add
+    print(f"You now have {curr_keychn} keychain.")  
+def rem_key():
+    global curr_keychn
+    rem=int(input(f"\nyou have {curr_keychn} keychain .How many want to remove:"))
+    curr_keychn-=rem
+    print(f"You now have {curr_keychn}")  
+def curr_key():
+    global curr_keychn
+    global sales_tax
+    global ship_cost
+    global per_cost
+    print(f"you have {curr_keychn} keychain\nKey chain cost ${cost} each.\nTotal cost is {cost*curr_keychn*(per_cost)}") 
+
+def chekout():
+    global curr_keychn
+    global cost
+    print("\nCHECKOUT")
+    name=input("\nEnter your name? ")
+    print(f"you have {curr_keychn} keychain.\nkeychain cost ${cost} each.\nTotal cost is ${cost+curr_keychn}.\nThanks for u r order {name}.")  
+for i in range(5):
+    print("\n\n1:Add keychain to order\n2:remove keychain from order\n3:view current order\n4:checkout")
+    k=int(input("\nEnter your choice:"))
+    if k==1:
+     add_key()
+    elif k==2:
+     rem_key()
+    elif k==3:
+     curr_key()
+    elif k==4:
+     chekout()
+     break
+    elif k>=5:
+       break
